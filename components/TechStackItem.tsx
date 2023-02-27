9326508277dbc9dc1b916ac261f78ae182cedab7
0xpaprhnds <yokeyeong@me.com>
2023-02-27 22:18:21 +0100

    Update TechStackItem.tsx

--- components/TechStackItem.tsx
+++ components/TechStackItem.tsx
@@ -7,7 +7,7 @@ const TechStackItem = ({ techStack }: { techStack: TechStack }) => {
       href={techStack.link}
       target="_blank"
       rel="noopener noreferrer"
-      className="flex flex-col items-center w-14 sm:w-16"
+      className="flex flex-col items-center underline underline-offset-2 hover:decoration-2 decoration-primary w-14 sm:w-16"
     >
       <div className="relative w-14 h-14 sm:w-16 sm:h-16">
         <Image
@@ -17,9 +17,7 @@ const TechStackItem = ({ techStack }: { techStack: TechStack }) => {
           className="object-contain"
         />
       </div>
-      <p className="text-sm font-light sm:text-base whitespace-nowrap">
-        {techStack.name}
-      </p>
+      <p className="text-sm sm:text-base whitespace-nowrap">{techStack.name}</p>
     </a>
   );
 };
